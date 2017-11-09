54a7f5a64b878e919515fa6912fb61d113e6ff31
samoyi <greatlining@163.com>
2017-11-09 17:58:11 +0800

    add some features

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,64 @@ gulp.task('uglify', function(){
 });
 
 
+// 从0开始对 indexRenamePic 文件夹中的文件以序号重命名
+// 使用时要保证indexRenamePic文件夹中只有待重命名的文件
+gulp.task('indexrename', function(){
+    let aNames = fs.readdirSync('tools/indexRename/'),
+        nFolderIndex = aNames.indexOf('renamed'),
+        sExt = '';
+    if(nFolderIndex===-1){
+        fs.mkdirSync('tools/indexRename/renamed/');
+    }
+    else{
+        aNames.splice(nFolderIndex, 1);
+    }
+    aNames.forEach((name, index)=>{
+        sExt = path.extname(name);
+        fs.copyFileSync('tools/indexRename/'+name, 'tools/indexRename/renamed/'+index+ sExt);
+    });
+});
+
+
+// 获得 filenames 文件夹里所有文件的名字，生成 names.txt 文件中并写入获得的文件名
+// exclude extension
+gulp.task('filenames', function(){
+    let aNames = fs.readdirSync('tools/filenames/');
+    aNames.forEach((name)=>{
+        let sNameWithoutExt = name.slice(0, name.indexOf('.'));
+        fs.appendFile('tools/filenames/names.txt', sNameWithoutExt + '\r\n');
+    });
+});
+// include extension
+gulp.task('filenames_ext', function(){
+    let aNames = fs.readdirSync('tools/filenames/');
+    aNames.forEach((name)=>{
+        fs.appendFile('tools/filenames/names_ext.txt', name + '\r\n');
+    });
+});
+
+
+// Classify by extension
+gulp.task('classifbyext', function(){
+    let aNames = fs.readdirSync('tools/classifyByExt/'),
+        aExt = [],
+        sExt = '';
+    aNames.forEach((name)=>{
+        sExt = path.extname(name).slice(1);
+        if(sExt){ // path.extname() can't get extension of those like .gitignore, will return ''
+            if( aExt.includes(sExt) ){
+                fs.renameSync('tools/classifyByExt/' +name, 'tools/classifyByExt/' +sExt+ '/' + name);
+            }
+            else{
+                aExt.push(sExt);
+                fs.mkdirSync('tools/classifyByExt/'+sExt);
+                fs.renameSync('tools/classifyByExt/' +name, 'tools/classifyByExt/' +sExt+ '/' + name);
+            }
+        }
+    });
+});
+
+
 // gulp.task('default',function(){
 //     gulp.watch('src/this.html',['htmlmin']);
 //     gulp.watch('src/this.scss',['sass']);
